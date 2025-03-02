e0627c4d29be85790791aa45ed5f92904c6b3729
Nicolas Tachet <nicolas.tachetdescombes@gmail.com>
2025-03-02 03:17:48 +0100

    Comments in service init

--- helpers/services/init.sh
+++ helpers/services/init.sh
@@ -3,14 +3,13 @@
 # Exit immediately if a command exits with a non-zero status
 set -e
 
-# Run service's specific initialization script
 cd "$APP_NAME/$SERVICE_NAME"
 
 # Copy environment variables
 cp .env.dist .env.dev
 cp .env.dist .env.prod
 
-# @todo : ? Cleanup service git
+# Remove git remote
 rm -rf "./.git"
 
 # Set service's specific scripts permissions
@@ -18,7 +17,7 @@ if [ -d "./scripts" ]; then
     chmod -R 755 "./scripts"
 fi
 
-# Service's specific script
+# Execute service's specific init script
 if [ -d "./scripts" ] && [ -f "./scripts/init.sh" ]; then
     echo -e "\e[33mRunning service specific initialization script...\e[0m"
     "./scripts/init.sh"
