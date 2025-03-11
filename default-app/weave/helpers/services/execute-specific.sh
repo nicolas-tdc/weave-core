b6d2ec64b13da444a07722f481e4a159bc68deb3
Nicolas Tachet <nicolas.tachetdescombes@gmail.com>
2025-03-11 21:21:11 +0100

    Fix cascade service execution for Weave path

--- default-app/weave/helpers/services/execute-specific.sh
+++ default-app/weave/helpers/services/execute-specific.sh
@@ -8,9 +8,9 @@ SCRIPT_NAME=$1
 # Stop all services
 for SERVICE_PATH in ./$SERVICES_DIR/*/; do
     # Check if it's a directory
-    if [ -d "$SERVICE_PATH" ] && [ -f "${SERVICE_PATH}$SCRIPT_NAME" ]; then
+    if [ -d "$SERVICE_PATH" ] && [ -f "${SERVICE_PATH}weave/$SCRIPT_NAME" ]; then
         cd "$SERVICE_PATH"
-        "./$SCRIPT_NAME"
+        "weave/$SCRIPT_NAME"
         cd - > /dev/null 2>&1
     fi
 done
