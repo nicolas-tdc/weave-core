4174f996de6fa23a62afc932d27bf3b12f3e27ce
Nicolas Tachet <nicolas.tachetdescombes@gmail.com>
2025-03-19 18:01:01 +0100

    Add weave update command (with submodules update)

--- weave.sh
+++ weave.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Exit immediately if a command exits with a non-zero status
+set -e
+
 current_dir=$(pwd)
 
 # Execute from project root
@@ -8,6 +11,7 @@ cd "$(dirname "$0")"
 case "$1" in
   create-app) ./scripts/create-app.sh ;;
   clone-app) ./scripts/clone-app.sh ;;
+  update) ./scripts/update.sh
 esac
 
 cd "$current_dir"
