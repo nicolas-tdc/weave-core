0240bf9d10e2cfe88c3195fc42322adb0cdee41a
Nicolas Tachet <nicolas.tachetdescombes@gmail.com>
2025-04-03 17:21:40 +0200

    Set application environment and source utils in main app weave file

--- default-app/weave/scripts/stop.sh
+++ default-app/weave/scripts/stop.sh
@@ -3,14 +3,6 @@
 # Exit immediately if a command fails
 set -e
 
-# Source utilities helpers
-if [ -f "./weave/helpers/utils.sh" ]; then
-    source ./weave/helpers/utils.sh
-else
-    echo -e "\e[31mCannot find 'utils' file! Exiting...\e[0m"
-    exit 1
-fi
-
 # Source services helpers
 if [ -f "./weave/helpers/services.sh" ]; then
     source ./weave/helpers/services.sh
@@ -18,8 +10,6 @@ else
     echo -e "\e[31mCannot find 'services' file! Exiting...\e[0m"
     exit 1
 fi
-
-set_application_environment
 
 # Required packages installation
 install_packages \
--- default-app/weave/scripts/update.sh
+++ default-app/weave/scripts/update.sh
@@ -3,14 +3,6 @@
 # Exit immediately if a command fails
 set -e
 
-# Source utilities helpers
-if [ -f "./weave/helpers/utils.sh" ]; then
-    source ./weave/helpers/utils.sh
-else
-    echo -e "\e[31mCannot find 'utils' file! Exiting...\e[0m"
-    exit 1
-fi
-
 # Source docker helpers
 if [ -f "./weave/helpers/docker.sh" ]; then
     source ./weave/helpers/docker.sh
@@ -26,8 +18,6 @@ else
     echo -e "\e[31mCannot find 'services' file! Exiting...\e[0m"
     exit 1
 fi
-
-set_application_environment
 
 echo -e "\e[33mTrying to update application '$APP_NAME'...\e[0m"
 
