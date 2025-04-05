857162fd7fa4b238da35b9b60dd5493629581246
Nicolas Tachet <nicolas.tachetdescombes@gmail.com>
2025-04-05 05:03:35 +0200

    Clean for hard reset

--- default-service/weave/scripts/backup-task.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Exit immediately if a command fails
-set -e
-
-# This script is used to manage a weave service's backup task.
-
-# Add backup task here
-echo -e "\e[33m$SERVICE_NAME: No backup task.\e[0m"
--- default-service/weave/scripts/update.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Exit immediately if a command fails
-set -e
-
-# This script is used to update a weave service.
-
-# Add update task here
-echo -e "\e[33m$SERVICE_NAME: No update task.\e[0m"
