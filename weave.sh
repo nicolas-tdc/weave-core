e33483234eefb521244e4d0e594bf1c31ed81230
Nicolas Tachet <nicolas.tachetdescombes@gmail.com>
2025-05-08 00:31:20 +0200

    Fix mispaste

--- weave.sh
+++ weave.sh
@@ -33,6 +33,22 @@ set -e
         exit 1
     fi
 
+    # Source utilities helpers
+    if [ -f "./weave-core/helpers/utils.sh" ]; then
+        source ./weave-core/helpers/utils.sh
+    else
+        echo -e "\e[31m$SERVICE_NAME: Cannot find 'utils' helper file. Exiting...\e[0m"
+        exit 1
+    fi
+
+    # Source docker helpers
+    if [ -f "./weave-core/helpers/docker.sh" ]; then
+        source ./weave-core/helpers/docker.sh
+    else
+        echo -e "\e[31m$SERVICE_NAME: Cannot find 'docker' helper file. Exiting...\e[0m"
+        exit 1
+    fi
+
     # Source services helpers
     if [ -f "./weave-core/helpers/services/execute.sh" ]; then
         source ./weave-core/helpers/services/execute.sh
@@ -41,9 +57,6 @@ set -e
         exit 1
     fi
 
-    # Defines APP_NAME, SERVICES_DIRECTORY, BACKUP_DIRECTORY
-    prepare_application
-
     # Check if enough arguments are provided
     if [ "$#" -lt 1 ]; then
         echo -e "\e[31mAt least one argument is required. Exiting...\e[0m"
@@ -51,46 +64,28 @@ set -e
         exit 1
     fi
 
-    # Parse arguments to extract options
-    read env_name service_name <<< $(parse_command_arguments "$@")
+    read APP_NAME SERVICES_DIRECTORY BACKUP_DIRECTORY <<< $(prepare_application)
+    export APP_NAME
+    export SERVICES_DIRECTORY
+    export BACKUP_DIRECTORY
+
+    # Parse arguments to extract environment and service name
+    read ENV_NAME SERVICE_NAME <<< $(parse_command_arguments "$@")
+    export ENV_NAME
+    export SERVICE_NAME
 
     # Aggregate relevant environment files
-    prepare_environment_files "$env_name"
+    prepare_environment_files "$ENV_NAME"
 
     # Execute the appropriate script based on command line argument
     command_name="$1"
     shift
     case "$command_name" in
-        run|kill|backup)
-            if [ "$service_name" == "" ]; then
-            execute_service_command $command_name $@
-            # script_relative_path="./weave-core/helpers/services/commands/$command_name.sh"
-            # script_path="$(cd "$(dirname "$script_relative_path")" && pwd)/$(basename "$script_relative_path")"
-
-            # if [ "$SERVICE_NAME" == "" ]; then
-            #     echo -e "\e[33mTrying to $command_name application '$APP_NAME'...\e[0m"
-
-            #     for service_path in $SERVICES_DIRECTORY/*/; do
-            #         SERVICE_NAME=$(basename "$service_path")
-
-            #         execute_service_command_script \
-            #             $script_path \
-            #             "$SERVICES_DIRECTORY/$SERVICE_NAME" \
-            #             $@
-            #     done
-            # else
-            #     echo -e "\e[33mTrying to $command_name service '$SERVICE_NAME'...\e[0m"
-
-            #     execute_service_command_script \
-            #         $script_path \
-            #         "$SERVICES_DIRECTORY/$SERVICE_NAME" \
-            #         $@
-
-            # fi
-            ;;
-        add-service) ./weave-core/commands/add-service.sh;;
-        backup-enable) ./weave-core/commands/backup-enable.sh;;
-        backup-disable) ./weave-core/commands/backup-disable.sh;;
+        run|kill) execute_command $command_name;;
+        backup) source ./weave-core/commands/backup.sh;;
+        add-service) source ./weave-core/commands/add-service.sh;;
+        backup-enable) source ./weave-core/commands/backup-enable.sh;;
+        backup-disable) source ./weave-core/commands/backup-disable.sh;;
         *)
             echo -e "\e[31mInvalid argument. Exiting...\e[0m"
             log_app_usage
